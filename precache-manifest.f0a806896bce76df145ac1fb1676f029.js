5d134ac912982784729dffca9bcc2f48b8a9ca68
unknown <mariia>
2019-07-10 21:22:04 +0300

    Updates

---
+++ precache-manifest.f0a806896bce76df145ac1fb1676f029.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c74838f6c3adb60147f5aeb66a5f94",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "2cb71550e32888014112",
+    "url": "/react_list-of-goods/static/css/main.404f6bfd.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react_list-of-goods/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "2cb71550e32888014112",
+    "url": "/react_list-of-goods/static/js/main.5f03a5a1.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
